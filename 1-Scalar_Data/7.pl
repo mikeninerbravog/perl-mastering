a200aeaee22de5dbe9b605953d2be7db7ea8019c
mikeninberbravog <bravog.bot2@gmail.com>
2023-04-21 10:59:04 -0300

    Hello Mike

---
+++ 1-Scalar_Data/7.pl
@@ -0,0 +1,11 @@
+my $a = 10;
+my $b = 5;
+
+my $sum = $a + $b;     # addition
+my $diff = $a - $b;    # subtraction
+my $prod = $a * $b;    # multiplication
+my $quot = $a / $b;    # division
+my $mod = $a % $b;     # modulus
+my $exp = $a ** $b;    # exponentiation
+
+print $diff."\n";
