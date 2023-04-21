1a896e49728b2653f6d257b98f1868b4fe601c62
mikeninberbravog <bravog.bot2@gmail.com>
2023-04-21 11:32:33 -0300

    Your branch is up to date with 'origin/retro-changes'.

--- 1-Scalar_Data/7.pl
+++
@@ -1,9 +0,0 @@
-my $a = 10;
-my $b = 5;
-
-my $sum = $a + $b;     # addition
-my $diff = $a - $b;    # subtraction
-my $prod = $a * $b;    # multiplication
-my $quot = $a / $b;    # division
-my $mod = $a % $b;     # modulus
-my $exp = $a ** $b;    # exponentiation
