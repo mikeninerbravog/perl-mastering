7cf3923c4c71927eeb8dd7ae0591a46df551da5a
mikeninberbravog <bravog.bot2@gmail.com>
2023-04-21 14:18:46 -0300

    Perl

---
+++ 1-Scalar_Data/1.pl
@@ -0,0 +1,4 @@
+# Scalar Data
+# Scalar data in Perl refers to a single value or item, such as a string, # number, or reference. Here's an example of scalar data:
+
+my $name = "John";  # a scalar variable storing a string value
